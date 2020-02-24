ef2b207440e5868aae74c083a30039d33a7bf6fa
Thomas Boerger <thomas@webhippie.de>
2020-02-24 10:23:54 +0100

    Add more common test naming

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,6 +7,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_gai_config_file(host):
+def test_config_file(host):
     file = host.file("/etc/gai.conf")
     assert file.contains("::ffff:0:0/96 100")
